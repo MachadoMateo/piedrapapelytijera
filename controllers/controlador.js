7470363b81089ca053a4bab2b1eb5e15be7549ca
MateoMachado <matius0125@live.com>
2023-05-17 12:57:30 -0500

    jugando piedra papel y tijera con el pc

---
+++ controllers/controlador.js
@@ -0,0 +1,19 @@
+let nombre="juan"
+let edad=33
+let yadesayuno=true
+let estatura=1.62
+let titulo=document.getElementById("titulo")
+let fotografia=document.getElementById("foto")
+let frutas=["piña","moras","fresas"]
+let comidas=Array("frijoles","carne asada","carne de cerdo")
+let trabajo={
+    cargo:"analista junior",
+    salario:"3800000"
+}
+console.log(trabajo)
+console.log(frutas)
+console.log(comidas)
+
+titulo.textContent="Bienvenido Mateo"
+titulo.classList.add("text-success")
+fotografia.src=""
